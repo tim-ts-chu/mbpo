c0edd7298a1df7506178316262b1fdf8dc3fe967
tim-ts-chu <tim.ts.chu@gmail.com>
2021-04-27 01:34:23 -0400

    mbpo baseline - torch impl

--- examples/config/ant/0.py
+++ examples/config/ant/0.py
@@ -11,7 +11,7 @@ params = {
     'kwargs': {
         'epoch_length': 1000,
         'train_every_n_steps': 1,
-        'n_train_repeat': 40,
+        'n_train_repeat': 1,
         # 'n_train_repeat': 20,
         'eval_render_mode': None,
         'eval_n_episodes': 1,
@@ -21,7 +21,8 @@ params = {
         'tau': 5e-3,
         'reward_scale': 1.0,
 
-        'model_train_freq': 250,
+        # 'model_train_freq': 250,
+        'model_train_freq': 1000,
         'model_retain_epochs': 1,
         'rollout_batch_size': 100e3,
         'deterministic': False,
